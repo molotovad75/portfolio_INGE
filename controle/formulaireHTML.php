50e01a7d3cbe84213a171d6d70381c778c27d561
Adrien <adrien.lac@tutanota.com>
2021-03-03 23:06:28 +0100

    formulaire implantée en BDD

--- controle/formulaireHTML.php
+++ controle/formulaireHTML.php
@@ -3,29 +3,22 @@
     // Variables de mon formulaire
     $nom=$_POST['nom'];
     $mail=$_POST['mail'];
-    $message=$_POST['message'];
+    $message=$_POST['_message'];
 
-    // Paramètres de ma BDD
-    $user='root';
-    $password='';
-    $reqSQL_insertion="INSERT INTO visiteur VALUES(:id_visiteur,:nom,:mail,:message)";
+    
+    $user="root";
+    $password="";
+    $reqSQL_insertion="INSERT INTO visiteur VALUES(:id_visiteur,:nom,:mail,:_message)";//Requête SQL permettant de stocker en BDD 
     try{
-        $BDD=new PDO('mysql:host=localhost;dbname=e-portfoliobdd;',$user,$password);
+        $BDD=new PDO('mysql:host=localhost;dbname=e-portfoliobdd;',$user,$password);//Mise en place de ma BDD
         $req=$BDD->prepare($reqSQL_insertion);
-        $req->execute(array('id_visiteur'=>null,'nom'=>$nom,'mail'=>$mail,'message'=>$message));
+        $req->execute(array('id_visiteur'=>null,'nom'=>$nom,'mail'=>$mail,'_message'=>$message));//On insère en BDD les données du visiteur
+        header("location:".  $_SERVER['HTTP_REFERER']); //On revient à la page d'accueil.
+       
+        
     }
     catch (Exception $e){
         die('Erreur : ' . $e->getMessage());
     }
-
-    
-    // function verif_mail($mail){
-    //     if(strstr($mail,'@') && ){
-
-    //     }elseif(){
-
-    //     }
-    // }
-
-
+    echo "Une réponse à votre message vous sera envoyé le plus vite possible !";
 ?>
