a602839622db776e1d1282ebb63f6262ef18a5e3
Adrien <adrien.lac@tutanota.com>
2021-04-14 23:29:28 +0200

    Prototypage des fonctions JS pour créer et animer les flèches

--- vue/JS/script.js
+++ vue/JS/script.js
@@ -2,3 +2,14 @@
 function message_apres_formulaire(){
     alert("Une réponse à votre message vous sera envoyé le plus vite possible !");
 }
+
+function lignebac_dut(){
+
+}
+function lignedut_ats(){
+
+}
+
+function ligne_ats_efrei(){
+
+}
