17c20f9843f40b775fe9168131bd22fad97cd685
Adrien <adrien.lac@tutanota.com>
2021-04-15 21:03:27 +0200

    Protypage des classes et des fonctions dans l'objectif de récupérer en BDD des infos servants à paramétrer le graphique en chart.JS

--- vue/JS/script.js
+++ vue/JS/script.js
@@ -3,12 +3,59 @@ function message_apres_formulaire(){
     alert("Une réponse à votre message vous sera envoyé le plus vite possible !");
 }
 
+  
+let nomtechno,niveautechno;
+
+class progrmmation{
+    constructor(nom,niveau){
+        nom=nomtechno;
+        niveau=niveautechno;
+    }
+}
+
+class dev_web{
+
+    constructor(nom,niveau){
+        nom=nomtechno;
+        niveau=niveautechno;
+    }
+}
+
+class systèmes_réseaux{
+
+    constructor(nom,niveau){    
+        nom=nomtechno;
+        niveau=niveautechno;
+    }
+}
+
+
+class BDD{
+
+    constructor(nom,niveau){
+        nom=nomtechno;
+        niveau=niveautechno;
+    }
+
+}
+
+
+function récupérerNom_techno(){
+    
+}
+
+function récupérerNiveau_techno(){
+
+}
+
+
 function lignebac_dut(){
 
 }
 function lignedut_ats(){
 
 }
+
 
 function ligne_ats_efrei(){
 
--- vue/JS/script_chart.js
+++ vue/JS/script_chart.js
@@ -19,30 +19,34 @@ function competences(){
         type: 'radar',
             data: 
             {
-                labels: ['JAVA Standard Edition', 'C/C++, C#, VB.net, VBA, Python', 'Git/GitHub', 'MVC, Bridge, décorateur'],
-                labels: ['HTML5/CSS3', 'PHP7, JAVA EE', 'JavaScript', 'Frameworks JS/PHP et bootstrap'],
+                
+                 labels: ['HTML5/CSS3', 'PHP7, JAVA EE', 'JavaScript', 'Frameworks JS/PHP et bootstrap'],
                 
                 datasets: [
                 {
                     data: [20, 33, 56, 12],
+                    // labels: ['HTML5/CSS3', 'PHP7, JAVA EE', 'JavaScript', 'Frameworks JS/PHP et bootstrap'],
                     label:"Dévelopement web",
                     backgroundColor: "rgba(0,28,255,0.5)",
                     borderColor:'rgba(19,255,0,0.5)',
                 },
                 {
                     data: [10, 43, 8, 5],
+                    // labels: ['JAVA Standard Edition', 'C/C++, C#, VB.net, VBA, Python', 'Git/GitHub', 'MVC, Bridge, décorateur'],
                     label:"Dévelopement logiciel",
                     backgroundColor: "rgba(19,255,0,0.5)",
                     borderColor:'rgba(0,28,255,0.5)',
                 },
                 {
                     data: [30, 3, 1, 28],
+                    // labels: ['SQL,PL/SQL', 'Microsoft Access, MySQL', 'MCD MLD', 'Extensions MERISE'],
                     label:"Base de données",
                     backgroundColor: "rgba(255,0,0,0.5)",
                     borderColor:'rgba(0,28,255,0.5)',
                 },
                 {
                     data: [7, 33, 27, 4],
+                    // labels: ['UNIX', 'Administration systèmes et réseaux', 'Modèle OSI', 'TCP/IP UDP/IP'],
                     label:"Systèmes et réseaux",
                     backgroundColor: " rgba(11,14,15,0.5)",
                     borderColor:'rgba(0,28,255,0.5)',
